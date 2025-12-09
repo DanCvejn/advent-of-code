2bad79a4ec4a2f8af24813a2b72c4b7a7193d8c6
dancvejn <dancvejn29@gmail.com>
2025-12-09 08:09:43 +0100

    renaming, day 5 part 1

---
+++ 2025/05/solution.php
@@ -0,0 +1,25 @@
+<?php
+
+$input = file_get_contents('input.txt');
+
+function partOne(string $input): int {
+  $sum = 0;
+  [$freshRanges, $availableIngs] = explode("\n\n", $input);
+  $freshIngsRanges = [];
+  foreach(explode("\n", $availableIngs) as $ingredient) {
+    $betweenFresh = false;
+    foreach(explode("\n", $freshRanges) as $range) {
+      $start = (int) explode("-", $range)[0];
+      $end = (int) explode("-", $range)[1];
+      if ($start <= (int) $ingredient && (int) $ingredient <= $end) {
+        $betweenFresh = true;
+        break;
+      }
+    }
+    if ($betweenFresh) $sum++;
+  }
+
+  return $sum;
+}
+
+echo partOne($input) . "\n";
